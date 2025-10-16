a96a3ffe1e9100a75ff9198deaab3170f1b31f08
kondetianugraha <kondetianugraha2004@gmail.com>
2025-10-16 18:34:21 +0530

    Task1

---
+++ Task1/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('alertBtn');
+  btn.addEventListener('click', function() {
+    alert("Hello! You clicked the button. Welcome to Web Development!");
+  });
+});
+ 
